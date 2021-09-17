7a5fa4f3a25b84181c4a020a8bd5e67e05ca7b72
KTWM <62528038+kelvintanwm@users.noreply.github.com>
2021-09-17 21:43:09 +0800

    Add files via upload

---
+++ Tradingbot.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import datetime as dt
+from pandas_datareader import data as pdr
+
+yf.pdr_override()
+
+stock = input("Enter a stock symbol ticker:")
+print(stock)
+
+start_year = 2021
+start_month = 1
+start_day = 1
+
+start = dt.datetime(start_year,start_month,start_day)
+
+now = dt.datetime.now()
+
+df = pdr.get_data_yahoo(stock,start,now)
+
+print(df)
+
+# ma = 50
+
+# sma_string = "Sma_{}".format(str(ma))
+
+
+# # Creating moving average using dataframe
+# df[sma_string] = df.iloc[:,4].rolling(window = ma).mean()
+
+# print(df)
+
+# df = df.iloc[ma:]
+
+# print(df)
+
+emas_used = [3,5,8,10,12,15,30,35,40,45,50,60]
+
+for x in emas_used:
+	ema = x
+	df["Ema_{}".format(ema)] = round(df.iloc[:,4].ewm(span = ema, adjust = False).mean(),2)
+
+print(df.tail())
+
+
+pos = 0
+num = 0
+percent_change = []
+
+for i in df.index:
+
+	cmin = min(df["Ema_3"][i],df["Ema_5"][i],df["Ema_8"][i],df["Ema_10"][i],df["Ema_12"][i],df["Ema_15"][i])
+	cmax = max(df["Ema_30"][i],df["Ema_35"][i],df["Ema_40"][i],df["Ema_45"][i],df["Ema_50"][i],df["Ema_60"][i])
+	
+	close = df["Adj Close"][i]
+
+	if(cmin > cmax):
+		print("Red White Blue")
+		if(pos == 0):
+			bp = close
+			pos = 1
+			print("Buying now at "+str(bp))
+
+	elif(cmin < cmax):
+		print("BLue White Red")
+		if(pos == 1):
+			pos = 0
+			sp = close
+			print("Selling now at "+str(sp))
+			pc = (sp/bp -1)*100
+			percent_change.append(pc)
+
+	if (num == df["Adj Close"].count()-1 and pos ==1 ):
+		pos = 0
+		sp = close
+		print("Selling now at "+str(sp))
+		pc = (sp/bp -1)*100
+		percent_change.append(pc)
+
+	num +=1
+
+print(percent_change)
+
+
+# Effectiveness of the trading strategy
+
+gains = 0
+gain_count = 0
+losses = 0
+loss_count = 0
+no_round = 1
+
+for i in percent_change:
+	if (i>0):
+		gains += i
+		gain_count +=1
+	else:
+		losses += i
+		loss_count +=1
+	no_round = no_round*((i/100)+1)
+
+no_round = round((no_round-1)*100,2)
+
+if (gain_count>0):
+	avg_gain = gains/gain_count
+	max_gain = str(max(percent_change))
+else:
+	avg_gain = 0
+	max_gain = "undefined"
+
+if (loss_count>0):
+	avg_loss = losses/loss_count
+	max_loss = str(min(percent_change))
+	ratio = str(-avg_gain/avg_loss)
+else:
+	avg_loss = 0
+	max_loss = "undefined"
+	ratio = "infinite"
+
+if (gain_count > 0 or loss_count>0):
+	batting_avg = gain_count/(gain_count+loss_count)
+else:
+	batting_avg = 0
+
+print()
+print("Results for "+ stock +" going back to "+str(df.index[0])+", Sample size: "+str(gain_count+loss_count)+" trades")
+print("EMAs used: "+str(emas_used))
+print("Batting Avg: "+ str(batting_avg))
+print("Gain/loss ratio: "+ ratio)
+print("Average Gain: "+ str(avg_gain))
+print("Average Loss: "+ str(avg_loss))
+print("Max Return: "+ max_gain)
+print("Max Loss: "+ max_loss)
+print("Total return over "+str(gain_count+loss_count)+ " trades: "+ str(no_round)+"%" )
+#print("Example return Simulating "+str(n)+ " trades: "+ str(nReturn)+"%" )
+print()
